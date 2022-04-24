f3b9e9a640d5815724ef9ce6eb72f112c93c35db
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-24 01:14:22 -0500

    feat: add card picker component

--- apps/cardpicker/src/App.tsx
+++ apps/cardpicker/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Shell } from 'ui'
+import {CardPicker} from './CardPicker'
 
 function App() {
   return (
     <Shell title='Card Picker'>
-      <div>Card picker</div>
+      <CardPicker />
     </Shell>
   )
 }
