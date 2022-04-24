60c67f38667dd518a1212704a89075313755ae7b
R Sánchez <9169831+remusa@users.noreply.github.com>
2022-04-24 01:04:50 -0500

    feat: add card picker app

---
+++ apps/cardpicker/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Shell } from 'ui'
+
+function App() {
+  return (
+    <Shell title='Card Picker'>
+      <div>Card picker</div>
+    </Shell>
+  )
+}
+
+export default App
